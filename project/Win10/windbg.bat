98e594e5ff102eaba51035c1b317df4e9049347a
Dale Stammen <dalestam@microsoft.com>
2017-04-26 08:03:42 -0700

    changed app id to Kodi

---
+++ project/Win10/windbg.bat
@@ -0,0 +1,6 @@
+"C:\Program Files (x86)\Windows Kits\10\Debuggers\x86\windbg.exe" -plmPackage ff29b046-290f-42a2-8a0f-0967977fdc36_1.0.0.0_x86__e8xk87pxx0yyw -plmApp Kodi -yD:\GitHub\xbmc\kodi-build\Debug\;D:\GitHub\xbmc\kodi-build\Debug\kodi-win10;
+
+
+
+
+
